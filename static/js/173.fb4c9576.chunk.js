08b851ed6fd7f8d791e45b1e82e22dce3b9bcd5d
Bhuwan Buda <bhuwanbuda143@gmail.com>
2022-11-04 22:11:15 +0545

    Updates

---
+++ static/js/173.fb4c9576.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[173],{4173:function(e,n,s){s.r(n);s(2791);var t=s(184);n.default=function(){return(0,t.jsx)("div",{children:"Aboutpage"})}}}]);
+//# sourceMappingURL=173.fb4c9576.chunk.js.map
