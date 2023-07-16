640544f76ff3aa8506a916f0f1d35b8fe958cdc4
Bhuwan Buda <bhuwanbuda143@gmail.com>
2023-07-16 17:22:04 +0545

    qr contact added

--- src/Components/HomePageComponents/HomepageImage.js
+++ src/Components/HomePageComponents/HomepageImage.js
@@ -8,42 +8,108 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+import ContactQr from "../ContactQr";
 import "./style.css";
 
 const links = [
   {
     id: 1,
-    icon: <FacebookRoundedIcon sx={{ color: "#3b5998" }} />,
+    icon: (
+      <FacebookRoundedIcon
+        sx={{
+          color: "#3b5998",
+          fontSize: "2rem",
+          background: "linear-gradient(90deg, #c3a2fe, #9358fd)",
+          borderRadius: "100%",
+          padding: "4px",
+        }}
+      />
+    ),
     name: "Facebook",
     href: "https://www.facebook.com/bhuwan.buda.1",
   },
   {
     id: 2,
-    icon: <GitHubIcon sx={{ color: "#333333" }} />,
+    icon: (
+      <GitHubIcon
+        sx={{
+          color: "#333333",
+          fontSize: "2rem",
+          background: "linear-gradient(90deg, #c3a2fe, #9358fd)",
+          borderRadius: "100%",
+          padding: "4px",
+        }}
+      />
+    ),
     name: "GitHub",
     href: "https://github.com/Bhuwan-Buda",
   },
   {
     id: 3,
-    icon: <InstagramIcon sx={{ color: "#8a3ab9" }} />,
+    icon: (
+      <InstagramIcon
+        sx={{
+          color: "#8a3ab9",
+          fontSize: "2rem",
+          background: "linear-gradient(90deg, #c3a2fe, #9358fd)",
+          borderRadius: "100%",
+          padding: "4px",
+        }}
+      />
+    ),
     name: "Instagram",
     href: "https://www.instagram.com/bhuwan.buda.1/",
   },
   {
     id: 4,
-    icon: <LinkedInIcon sx={{ color: "#0e76a8 " }} />,
+    icon: <ContactQr />,
+    name: "Add to contact"
+  },
+  {
+    id: 5,
+    icon: (
+      <LinkedInIcon
+        sx={{
+          color: "#0e76a8",
+          fontSize: "2rem",
+          background: "linear-gradient(90deg, #c3a2fe, #9358fd)",
+          borderRadius: "100%",
+          padding: "4px",
+        }}
+      />
+    ),
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/bhuwan-buda-a7538b180/",
   },
   {
-    id: 5,
-    icon: <EmailIcon sx={{ color: "#8a3ab9" }} />,
+    id: 6,
+    icon: (
+      <EmailIcon
+        sx={{
+          color: "#8a3ab9",
+          fontSize: "2rem",
+          background: "linear-gradient(90deg, #c3a2fe, #9358fd)",
+          borderRadius: "100%",
+          padding: "4px",
+        }}
+      />
+    ),
     name: "Mail",
     href: "mailto:bhuwanbuda143@gmail.com",
   },
   {
-    id: 6,
-    icon: <PhoneInTalkIcon sx={{ color: "#444444 " }} />,
+    id: 7,
+    icon: (
+      <PhoneInTalkIcon
+        sx={{
+          color: "#444444",
+          fontSize: "2rem",
+          background: "linear-gradient(90deg, #c3a2fe, #9358fd)",
+          borderRadius: "100%",
+          padding: "4px",
+        }}
+      />
+    ),
     name: "Call",
     href: "tel:9868792901",
   },
@@ -79,7 +145,7 @@ const HomepageImage = () => {
           <br /> to deliver exceptional customer experience.
         </Typography>
       </Grid>
-      <Grid item xs={12} className="social-links-container" data-aos="zoom-in">
+      <Grid item xs={12} className="social-links-container">
         <SpeedDial
           ariaLabel="SpeedDial for links"
           FabProps={{ variant: "extended" }}
@@ -87,15 +153,15 @@ const HomepageImage = () => {
           direction="up"
           className="speedDial-container"
         >
-          {links.map((link) => (
+          {links?.map((link) => (
             <SpeedDialAction
               key={link.id}
               icon={link.icon}
-              tooltipTitle={link.name}
+              tooltipTitle={link.name ? link.name : ""}
               component="a"
               target="_blank"
               tooltipPlacement="top"
-              href={link.href ? link.href : ""}
+              href={link.href ? link.href : undefined}
             />
           ))}
         </SpeedDial>
